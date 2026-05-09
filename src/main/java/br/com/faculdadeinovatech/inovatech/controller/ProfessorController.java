af7c2d2f3946bf140275b5ae267163ce924e839b
Gabriel371-tech <gabriel.lopes176@etec.sp.gov.br>
2026-05-09 13:42:40 -0300

    fix: arrumando erros

--- src/main/java/br/com/faculdadeinovatech/inovatech/controller/ProfessorController.java
+++ src/main/java/br/com/faculdadeinovatech/inovatech/controller/ProfessorController.java
@@ -1,10 +1,13 @@
 package br.com.faculdadeinovatech.inovatech.controller;
 
 import br.com.faculdadeinovatech.inovatech.entity.Professor;
+import br.com.faculdadeinovatech.inovatech.service.CursoService;
 import br.com.faculdadeinovatech.inovatech.service.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
